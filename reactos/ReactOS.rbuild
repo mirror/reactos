mirror/reactos

commit 0a6efc84b97944bb7d39412f9d93a7f8c516468a

Place .h files generated from .idl definitions in the intermediate directory
git-svn-id: svn://svn.reactos.org/reactos/trunk@27269 97493ccd-5924-5043-b1f5-66cb403b36ce

reactos/ReactOS.rbuild
@@ -62,6 +62,7 @@
   <include>.</include>
   <include>include</include>
   <include>include/psdk</include>
+  <include base="__intermediate">include/psdk</include>
   <include>include/dxsdk</include>
   <include>include/crt</include>
   <include>include/ddk</include>

reactos/tools/rbuild/backend/mingw/modulehandler.cpp
@@ -323,6 +323,8 @@ MingwModuleHandler::GetActualSourceFilename (
 		else //if ( module.type == IdlHeader )
 		{
 			newname = basename + ".h";
+			PassThruCacheDirectory ( NormalizeFilename ( newname ),
+				                     backend->intermediateDirectory );
 			return new FileLocation ( fileLocation->directory, filename );
 		}
 	}
@@ -341,7 +343,7 @@ MingwModuleHandler::GetExtraDependencies (
 		if ( (module.type == RpcServer) || (module.type == RpcClient) )
 			return GetRpcServerHeaderFilename ( basename ) + " " + GetRpcClientHeaderFilename ( basename );
 		else
-			return GetIdlHeaderFilename ( basename );			
+			return GetIdlHeaderFilename ( basename );
 	}
 	else
 		return "";
@@ -522,9 +524,6 @@ MingwModuleHandler::GetObjectFilename (
 	else
 		directoryTree = backend->intermediateDirectory;
 
-	if (newExtension == ".h")
-		directoryTree = NULL;
-
 	string obj_file = PassThruCacheDirectory (
 		NormalizeFilename ( ReplaceExtension (
 			RemoveVariables ( sourceFilename ),
@@ -679,13 +678,20 @@ MingwModuleHandler::ConcatenatePaths (
 /* static */ string
 MingwModuleHandler::GenerateGccIncludeParametersFromVector ( const vector<Include*>& includes )
 {
-	string parameters;
+	string parameters, path_prefix;
 	for ( size_t i = 0; i < includes.size (); i++ )
 	{
 		Include& include = *includes[i];
 		if ( parameters.length () > 0 )
 			parameters += " ";
-		parameters += "-I" + include.directory;
+		if ( include.baseValue == "__intermediate" )
+			path_prefix = backend->intermediateDirectory->name + cSep;
+		else if (include.baseValue == "__output" )
+			path_prefix = backend->outputDirectory->name + cSep;
+		else
+			path_prefix = "";
+
+		parameters += "-I" + path_prefix + include.directory;
 	}
 	return parameters;
 }
@@ -1234,7 +1240,8 @@ MingwModuleHandler::GetRpcClientHeaderFilename ( string basename ) const
 string
 MingwModuleHandler::GetIdlHeaderFilename ( string basename ) const
 {
-	return basename + ".h";
+	return PassThruCacheDirectory ( basename + ".h",
+	                                backend->intermediateDirectory );
 }
 
 void

reactos/tools/rbuild/include.cpp
@@ -52,6 +52,7 @@ Include::Include ( const Project& project,
 {
 	this->directory = NormalizeFilename ( basePath + sSep + directory );
 	this->basePath = NormalizeFilename ( basePath );
+	this->baseValue = basePath;
 }
 
 Include::~Include ()
@@ -65,26 +66,40 @@ Include::ProcessXML()
 	att = node->GetAttribute ( "base", false );
 	if ( att )
 	{
-		if ( !module )
-			throw XMLInvalidBuildFileException (
-				node->location,
-				"'base' attribute illegal from global <include>" );
 		bool referenceResolved = false;
-		if ( att->value == project.name )
+		baseValue = att->value;
+
+		if ( !module )
 		{
-			basePath = ".";
-			referenceResolved = true;
-		}
-		else
-		{
-			const Module* base = project.LocateModule ( att->value );
-			if ( base != NULL )
-			{
-				baseModule = base;
-				basePath = base->GetBasePath ();
+			if ( att->value == "__intermediate" || att->value == "__output" )
 				referenceResolved = true;
+			else
+			{
+				throw XMLInvalidBuildFileException (
+					node->location,
+					"'base' attribute illegal from global <include>" );
 			}
 		}
+
+		if ( !referenceResolved )
+		{
+			if ( att->value == project.name )
+			{
+				basePath = ".";
+				referenceResolved = true;
+			}
+			else
+			{
+				const Module* base = project.LocateModule ( att->value );
+				if ( base != NULL )
+				{
+					baseModule = base;
+					basePath = base->GetBasePath ();
+					referenceResolved = true;
+				}
+			}
+		}
+
 		if ( !referenceResolved )
 			throw XMLInvalidBuildFileException (
 				node->location,

reactos/tools/rbuild/rbuild.h
@@ -366,6 +366,7 @@ public:
 	const Module* baseModule;
 	std::string directory;
 	std::string basePath;
+	std::string baseValue;
 
 	Include ( const Project& project,
 	          const XMLElement* includeNode );

commit 104b1624dc60de79217d6400280a7926eb65d7a6

Fix the build system, accidently broke it with r27269
git-svn-id: svn://svn.reactos.org/reactos/trunk@27276 97493ccd-5924-5043-b1f5-66cb403b36ce

reactos/ReactOS.rbuild
@@ -62,7 +62,7 @@
   <include>.</include>
   <include>include</include>
   <include>include/psdk</include>
-  <include base="__intermediate">include/psdk</include>
+  <include root="intermediate">include/psdk</include>
   <include>include/dxsdk</include>
   <include>include/crt</include>
   <include>include/ddk</include>

reactos/dll/win32/hhctrl.ocx/hhctrl.ocx.rbuild
@@ -2,6 +2,7 @@
 	<importlibrary definition="hhctrl.ocx.spec.def" />
 	<include base="hhctrl">.</include>
 	<include base="ReactOS">include/reactos/wine</include>
+	<include base="ReactOS" root="intermediate">include/reactos/wine</include>
 	<define name="__REACTOS__" />
 	<define name="__WINESRC__" />
 	<define name="__USE_W32API" />

reactos/dll/win32/ole32/ole32.rbuild
@@ -2,6 +2,7 @@
 	<autoregister infsection="OleControlDlls" type="DllRegisterServer" />
 	<importlibrary definition="ole32.spec.def" />
 	<include base="ole32">.</include>
+	<include base="ole32" root="intermediate">.</include>
 	<include base="ReactOS">include/reactos/wine</include>
 	<define name="__REACTOS__" />
 	<define name="__WINESRC__" />

reactos/tools/rbuild/backend/mingw/modulehandler.cpp
@@ -684,9 +684,9 @@ MingwModuleHandler::GenerateGccIncludeParametersFromVector ( const vector<Includ
 		Include& include = *includes[i];
 		if ( parameters.length () > 0 )
 			parameters += " ";
-		if ( include.baseValue == "__intermediate" )
+		if ( include.root == "intermediate" )
 			path_prefix = backend->intermediateDirectory->name + cSep;
-		else if (include.baseValue == "__output" )
+		else if (include.root == "output" )
 			path_prefix = backend->outputDirectory->name + cSep;
 		else
 			path_prefix = "";
@@ -797,7 +797,7 @@ MingwModuleHandler::GenerateMacro (
 	for ( i = 0; i < data.includes.size(); i++ )
 	{
 		const Include& include = *data.includes[i];
-		string includeDirectory;
+		string includeDirectory, path_prefix;
 		if ( include.baseModule != NULL &&
 		     ( include.baseModule->type == RpcServer ||
 		       include.baseModule->type == RpcClient ||
@@ -806,9 +806,18 @@ MingwModuleHandler::GenerateMacro (
 	                                                            backend->intermediateDirectory );
 		else
 			includeDirectory = include.directory;
+
+		if ( include.root == "intermediate" )
+			path_prefix = backend->intermediateDirectory->name + cSep;
+		else if (include.root == "output" )
+			path_prefix = backend->outputDirectory->name + cSep;
+		else
+			path_prefix = "";
+
 		fprintf (
 			fMakefile,
-			" -I%s",
+			" -I%s%s",
+			path_prefix.c_str(),
 			includeDirectory.c_str() );
 	}
 	for ( i = 0; i < data.defines.size(); i++ )

reactos/tools/rbuild/include.cpp
@@ -52,7 +52,6 @@ Include::Include ( const Project& project,
 {
 	this->directory = NormalizeFilename ( basePath + sSep + directory );
 	this->basePath = NormalizeFilename ( basePath );
-	this->baseValue = basePath;
 }
 
 Include::~Include ()
@@ -67,18 +66,12 @@ Include::ProcessXML()
 	if ( att )
 	{
 		bool referenceResolved = false;
-		baseValue = att->value;
 
 		if ( !module )
 		{
-			if ( att->value == "__intermediate" || att->value == "__output" )
-				referenceResolved = true;
-			else
-			{
-				throw XMLInvalidBuildFileException (
-					node->location,
-					"'base' attribute illegal from global <include>" );
-			}
+			throw XMLInvalidBuildFileException (
+				node->location,
+				"'base' attribute illegal from global <include>" );
 		}
 
 		if ( !referenceResolved )
@@ -109,4 +102,18 @@ Include::ProcessXML()
 	}
 	else
 		directory = NormalizeFilename ( node->value );
+
+	att = node->GetAttribute ( "root", false );
+	if ( att )
+	{
+		if ( att->value != "intermediate" && att->value != "output" )
+		{
+			throw InvalidAttributeValueException (
+				node->location,
+				"root",
+				att->value );
+		}
+
+		root = att->value;
+	}
 }

reactos/tools/rbuild/rbuild.h
@@ -366,7 +366,7 @@ public:
 	const Module* baseModule;
 	std::string directory;
 	std::string basePath;
-	std::string baseValue;
+	std::string root;
 
 	Include ( const Project& project,
 	          const XMLElement* includeNode );